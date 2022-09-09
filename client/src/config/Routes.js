b506c433a5ecb96688d98086e4bd5bdf698b88a1
qmsparks <q.m.sparks@gmail.com>
2022-09-09 16:02:15 -0600

    test

--- client/src/config/Routes.js
+++ client/src/config/Routes.js
@@ -1,7 +1,15 @@
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom';
+import Home from '../pages/Home';
+import Profile from '../pages/Profile';
+import Signup from '../pages/Signup';
+import Login from '../pages/Login';
 
 function Routes() {
+  return(
+    <Routes>
 
+    </Routes>
+  )
 }
 
 export default Routes;
